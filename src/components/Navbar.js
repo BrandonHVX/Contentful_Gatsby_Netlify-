c34753113078707d212e637b8ef468fdddb5999a
Brandon <brandongines@gmail.com>
2020-08-17 20:39:19 -0400

    updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,7 +25,7 @@ export default class Header extends React.Component {
             <Navbar.Collapse id="navbarResponsive">
               <Nav className="navbar-nav ml-auto my-2 my-lg-0">
                 <Scrollspy className="navbar-nav"
-                           items={["about", "education","films", "gallery" ]}
+                           items={["about", "education","films", "blog", "gallery",  ]}
                            currentClassName="active" rootEl={"#mainNav"} offset={-75}>
                   <li className="nav-item">
                     <Nav.Link className={"js-scroll-trigger"} href="#about" onClick={Scroller.handleAnchorScroll}>About</Nav.Link>
@@ -36,12 +36,13 @@ export default class Header extends React.Component {
                   <li className="nav-item">
                     <Nav.Link className={"js-scroll-trigger"} href="#films" onClick={Scroller.handleAnchorScroll}>Films</Nav.Link>
                   </li>
-                  <li className="nav-item">
-                    <Nav.Link className={"js-scroll-trigger"} href="#gallery" onClick={Scroller.handleAnchorScroll}>Gallery</Nav.Link>
-                  </li>
+               
                 
                   <li className="nav-item">
-                    <Nav.Link className={"js-scroll-trigger"} href="#portfolio" onClick={Scroller.handleAnchorScroll}>Blog</Nav.Link>
+                    <Nav.Link className={"js-scroll-trigger"} href="#blog" onClick={Scroller.handleAnchorScroll}>Blog</Nav.Link>
+                  </li>
+                  <li className="nav-item">
+                    <Nav.Link className={"js-scroll-trigger"} href="#gallery" onClick={Scroller.handleAnchorScroll}>Gallery</Nav.Link>
                   </li>
                   <li className="nav-item">
                     <Nav.Link className={"js-scroll-trigger"} href="#contact" onClick={Scroller.handleAnchorScroll}>Contact</Nav.Link>
